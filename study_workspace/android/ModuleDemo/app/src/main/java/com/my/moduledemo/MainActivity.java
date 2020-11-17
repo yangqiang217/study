b1bbee22d315e813afe4fc675db6b7419cd6917b
yangqiang <yangqiang149@163.com>
2020-11-17 21:16:53 +0800

    edit code

--- study_workspace/android/ModuleDemo/app/src/main/java/com/my/moduledemo/MainActivity.java
+++ study_workspace/android/ModuleDemo/app/src/main/java/com/my/moduledemo/MainActivity.java
@@ -3,11 +3,15 @@ package com.my.moduledemo;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.my.common_lib.Utils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Utils.printInfo();
     }
 }
---
+++ study_workspace/android/ModuleDemo/common-lib/src/main/java/com/my/common_lib/Utils.java
@@ -0,0 +1,7 @@
+package com.my.common_lib;
+
+public class Utils {
+    public static void printInfo() {
+        System.out.println("shit");
+    }
+}
