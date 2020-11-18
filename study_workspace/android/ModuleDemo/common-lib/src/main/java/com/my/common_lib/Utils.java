30aeb4b7c11663fcb44afebdd3a7546f107af3dd
yangqiang <yangqiang149@163.com>
2020-11-18 19:35:27 +0800

    edit code

--- study_workspace/android/ModuleDemo/common-lib/src/main/java/com/my/common_lib/Utils.java
+++ study_workspace/android/ModuleDemo/common-lib/src/main/java/com/my/common_lib/Utils.java
@@ -1,7 +1,10 @@
 package com.my.common_lib;
 
+import org.greenrobot.eventbus.EventBus;
+
 public class Utils {
     public static void printInfo() {
         System.out.println("shit");
+        EventBus.getDefault().register(new Utils());
     }
 }
