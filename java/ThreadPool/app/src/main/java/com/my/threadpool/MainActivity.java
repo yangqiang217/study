d84d5211e515fc49c10916154bb4614863902e93
yangqiang <yangqiang149@163.com>
2020-12-13 19:43:47 +0800

    edit code

--- java/ThreadPool/app/src/main/java/com/my/threadpool/MainActivity.java
+++ java/ThreadPool/app/src/main/java/com/my/threadpool/MainActivity.java
@@ -5,23 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ExecutorService executorService;
+    private ExecutorService mExecutorService;
+    private BlockingQueue<Runnable> mQueue;
+
+    private Thread mThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //        ExecutorService executorService = Executors.newCachedThreadPool();
-//        ExecutorService executorService = Executors.newFixedThreadPool(3);
-        executorService = new ThreadPoolExecutor(1, 3, 5L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+//        mQueue = new LinkedBlockingQueue<>();
+        mQueue = new SynchronousQueue<>();
+
+        mThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                L.d("in thread start");
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                L.d("in thread end");
+            }
+        });
+
+//        mExecutorService = Executors.newCachedThreadPool();
+//        mExecutorService = Executors.newFixedThreadPool(3);
+        mExecutorService = Executors.newSingleThreadExecutor();
+//        mExecutorService = new ThreadPoolExecutor(1, 3, 5L, TimeUnit.SECONDS, mQueue);
 
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,10 +54,17 @@ public class MainActivity extends AppCompatActivity {
                 add();
             }
         });
+
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start();
+            }
+        });
     }
 
     private void add() {
-        executorService.execute(new Runnable() {
+        mExecutorService.submit(new Runnable() {
             @Override
             public void run() {
                 L.d("start");
@@ -41,8 +73,29 @@ public class MainActivity extends AppCompatActivity {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                String a = null;
+                System.out.println(a.toString());
                 L.d("finish");
             }
         });
+        L.d("queue size: " + mQueue.size());
+
+//        mExecutorService.submit(new Callable<Object>() {
+//            @Override
+//            public Object call() throws Exception {
+//                L.d("start");
+//                try {
+//                    TimeUnit.SECONDS.sleep(3);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                L.d("finish");
+//                return "shit";
+//            }
+//        });
+    }
+
+    private void start(){
+        mThread.start();
     }
 }
