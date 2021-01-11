27248a1edce9d2da522396f2f254e6993b774dba
yangqiang <yangqiang149@163.com>
2021-01-12 00:00:16 +0800

    edit code

--- kotlin/KotlinStudy/src/main/kotlin/_4面向对象/2_非默认构造方法或属性的类.kt
+++ kotlin/KotlinStudy/src/main/kotlin/_4面向对象/2_非默认构造方法或属性的类.kt
@@ -6,6 +6,10 @@ fun main() {
 
     val u2 = User2("name")
     println(u2.nickName)
+
+    val u4 = User4("name4")
+    u4.address = "add"
+    println(u4.address)
 }
 /**
  * 直接写在类后面()里的东西就是主构造方法
@@ -57,11 +61,46 @@ class Button3(ctx: String) : View(ctx) {
 }
 
 /**
- * 接口可以包含抽象属性声明
+ * 接口包含抽象属性声明
  */
 interface User3 {
     val nickName: String
+    val age: Int
+        get() = nickName.length//有get子类就不用初始化了
 }
-class PrivateUser : User3 {
+class PrivateUser(override val nickName: String) : User3 {
 
+}
+class SubscribingUser(val email: String) : User3 {
+    override val nickName: String
+        get() = email.substringBefore("@")//自定义getter
+}
+class FacebookUser(val accountId: Int): User3 {
+    override val nickName: String = getFacebookName(accountId)
+
+    private fun getFacebookName(accountId: Int): String {
+        return ""
+    }
+}
+
+/**
+ * getter setter
+ */
+class User4(val name: String) {
+    var address: String = "null"
+        set(value) {//u4.address = "add"会调用set
+            println("add changed")
+            field = value
+        }
+        get() {
+            println("get called")
+            return "shit"
+        }
+    var age: Int = 0
+        private set(value) {//不能再外面修改此属性，编译报错
+            field = value
+        }
+        /*private getter不能是private的，会报错 */get() {
+            return 1
+        }
 }
---
+++ kotlin/KotlinStudy/src/main/kotlin/_4面向对象/3_数据类和类委托.kt
@@ -0,0 +1,26 @@
+package _4面向对象
+
+fun main() {
+    val client2 = Client2("name", 1)
+    println(client2)
+}
+
+class Client(val name: String, val postalCode: Int) {
+    override fun toString(): String {
+        return "Client(name=$name, postalCode=$postalCode)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null || other !is Client)
+            return false
+        return name == other.name && postalCode == other.postalCode
+    }
+
+    //复制val变量的类
+    fun copy(name: String = this.name, postalCode: Int = this.postalCode) = Client(name, postalCode)
+}
+
+/**
+ * data类会自动重写满足Client中要求的toString、equals、hashCode
+ */
+data class Client2(val name: String, val postalCode: Int)
