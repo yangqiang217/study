9c92c6d663942c65b5c27ab492ab8c2cbc619e2d
yangqiang <yangqiang03@corp.netease.com>
2021-01-12 20:55:55 +0800

    edit code

--- kotlin/KotlinStudy/src/main/kotlin/_4面向对象/3_数据类和类委托.kt
+++ kotlin/KotlinStudy/src/main/kotlin/_4面向对象/3_数据类和类委托.kt
@@ -24,3 +24,38 @@ class Client(val name: String, val postalCode: Int) {
  * data类会自动重写满足Client中要求的toString、equals、hashCode
  */
 data class Client2(val name: String, val postalCode: Int)
+
+
+/**
+ * 委托 by
+ * 常规委托实现：挨个把动作给内部变量
+ */
+class DelegatingCollection<T> : Collection<T> {
+    private val innerList = arrayListOf<T>()
+
+    override val size: Int
+        get() = innerList.size
+    override fun isEmpty(): Boolean = innerList.isEmpty()
+    override fun contains(element: T): Boolean = innerList.contains(element)
+    override fun iterator(): Iterator<T> = innerList.iterator()
+    override fun containsAll(elements: Collection<T>): Boolean = innerList.containsAll(elements)
+}
+/** 用by实现简写的委托 */
+class DelegatingCollection2<T>(innerList: Collection<T> = ArrayList<T>()) : Collection<T> by innerList {
+}
+/**
+ * 使用委托实例
+ * 只实现add和addAll，其它的比如contains、isEmpty等不用写也不会报错，因为委托了
+ */
+class CountingSet<T>(val innerSet: MutableCollection<T> = HashSet<T>()) : MutableCollection<T> by innerSet {
+    var objectAdded = 0
+    override fun add(element: T): Boolean {
+        objectAdded++
+        return innerSet.add(element)
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        objectAdded += elements.size
+        return innerSet.addAll(elements)
+    }
+}
--- kotlin/KotlinStudy/src/main/kotlin/main.kt
+++ kotlin/KotlinStudy/src/main/kotlin/main.kt
@@ -2,6 +2,6 @@ import _3函数.LINE
 import _3函数.lastChar
 
 fun main(args: Array<String>) {
-    println(LINE)
-    println("".lastChar())
+    val s = "abc"
+    println(s.toList())
 }
