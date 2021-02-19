ce0528b662eb0012e9a342a4895255481be93e4f
yangqiang <yangqiang149@163.com>
2021-02-19 23:16:11 +0800

    edit code

--- android/KotlinStudy/app/src/main/java/com/my/kotlinstudy/MainActivity.kt
+++ android/KotlinStudy/app/src/main/java/com/my/kotlinstudy/MainActivity.kt
@@ -2,10 +2,21 @@ package com.my.kotlinstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.my.kotlinstudy._10协程.multiCoroutine
+import com.my.kotlinstudy._10协程.singleCoroutine
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn1.setOnClickListener {
+            singleCoroutine()
+        }
+
+        btn2.setOnClickListener {
+            multiCoroutine()
+        }
     }
 }
