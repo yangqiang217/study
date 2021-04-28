aa53addf1f5d366d336fea1b7273011b325ed772
yangqiang <yangqiang149@163.com>
2021-04-28 19:26:56 +0800

    edit code

---
+++ android/OpenCV/app/src/main/java/com/my/opencv/app/AppUtils.java
@@ -0,0 +1,12 @@
+package com.my.opencv.app;
+
+import org.opencv.core.Scalar;
+
+/**
+ * Created by Noureldien on 5/9/2015.
+ */
+public class AppUtils {
+
+    public static Scalar ThemeColor = new Scalar(0, 173, 142);
+
+}
